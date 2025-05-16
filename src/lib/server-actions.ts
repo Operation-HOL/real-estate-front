b8e3626ee7357f7645b15af867887e42b9947932
qusee <somelelequse03@icloud.com>
2025-05-16 03:35:28 +0200

    comment

--- src/lib/server-actions.ts
+++ src/lib/server-actions.ts
@@ -1,5 +1,6 @@
 export async function searchProperties(query: string) {
     try {
+        // querying the search service
         const res = await fetch("http://localhost:8081/api/v1/search?query=" + query);
         const data = await res.json();
         return data;
