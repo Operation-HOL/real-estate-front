4658c9efd516c6a96110e09835ebe1f4f32d84e5
qusee <somelelequse03@icloud.com>
2025-05-27 00:47:30 +0200

    configured urls

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,10 @@
+const DEPLOYED_BACKEND_URL = "https://property-service-2mkg.onrender.com";
+const LOCAL_BACKEND_URL = "http://localhost:8080";
+
 export const fetchAllProperties = async () => {
     try {
         // for now fetch straight from the server, without using the api
-        const res = await fetch("http://localhost:8080/property/all");
+        const res = await fetch(`${DEPLOYED_BACKEND_URL}/property/all`);
         return await res.json();
     } catch (error) {
         throw error;
